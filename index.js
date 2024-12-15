95f472f67d3b12e44c6b43424345dc08c18c3e0a
JMarSp962 <jmsp3010@gmail.com>
2024-12-14 21:48:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["✅𝗕𝗼𝘁 𝗼𝗳𝗶𝗰𝗶𝗮𝗹 𝗱𝗲 𝗥𝗔𝗚𝗘", "🎮 Playing VALORANT"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
